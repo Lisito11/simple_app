f95d1b1aeb89cc4cfb091f11c1f300126666b48d
Lisito11 <l.andrespg11@gmail.com>
2021-10-07 00:21:05 -0400

    Add 3 new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simple_app/src/widgets/home_screen.dart';
+import 'package:simple_app/src/pages/home_screen.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/src/widgets/actual_date_widget.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ActualDateWidget extends StatefulWidget {
+  @override
+  _ActualDateWidgetState createState() => _ActualDateWidgetState();
+}
+
+class _ActualDateWidgetState extends State<ActualDateWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
